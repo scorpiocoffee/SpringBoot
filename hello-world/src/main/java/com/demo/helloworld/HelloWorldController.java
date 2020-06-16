c32b24d23cd8ab2d3c570c85362c63a2622376dd
Shao <sherry.shao01@sap.com>
2020-06-16 16:18:57 +0800

    first spring boot app -- hello world

---
+++ hello-world/src/main/java/com/demo/helloworld/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.demo.helloworld;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
